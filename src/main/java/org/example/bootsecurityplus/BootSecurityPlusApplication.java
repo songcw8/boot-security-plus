195a7012e532bf97ec2117187df4c4c39013557c
songcw8 <ben990158@gmail.com>
2025-04-11 09:47:59 +0900

    Initial commit

---
+++ src/main/java/org/example/bootsecurityplus/BootSecurityPlusApplication.java
@@ -0,0 +1,13 @@
+package org.example.bootsecurityplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSecurityPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootSecurityPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bootsecurityplus/BootSecurityPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bootsecurityplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootSecurityPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
