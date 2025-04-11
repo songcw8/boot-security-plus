71e5cb764dad64063ac92b0d9e394b74cc08740a
songcw8 <ben990158@gmail.com>
2025-04-11 11:13:16 +0900

    security 비활성화

---
+++ src/main/java/org/example/bootsecurityplus/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package org.example.bootsecurityplus.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+
+}
